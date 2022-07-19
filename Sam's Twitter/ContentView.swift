b75cee40a42628238eafda3eab257b82f043a5ba
Sam <samgrover221692@gmail.com>
2022-07-19 19:52:50 +0530

    Initial Commit

---
+++ Sam's Twitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sam's Twitter
+//
+//  Created by Sam Grover on 19/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sam's Twitter/Sam_s_TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sam_s_TwitterApp.swift
+//  Sam's Twitter
+//
+//  Created by Sam Grover on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Sam_s_TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
