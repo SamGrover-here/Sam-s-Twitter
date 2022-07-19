44c667e3210e7cc053ab732e4f7b7c6d513b838d
Sam <samgrover221692@gmail.com>
2022-07-19 23:08:06 +0530

    - Complete profile header View - Made all extentions to the Profile view

--- Sam's Twitter/Core/Profile/Views/ProfileView.swift
+++ Sam's Twitter/Core/Profile/Views/ProfileView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack{
-            HeaderView
-            //extention Made Below
-            ActionButtons
+        VStack(alignment: .leading){
             
+//extention Made Below
+            
+            //header
+            HeaderView
+            // action buttons: bell and edit profile
+            ActionButtons
+            //User info
+            UserInfoDetail
             Spacer()
         }
     }
@@ -62,7 +67,8 @@ extension ProfileView{
                 
             } label: {
                 Text("Edit Profile")
-                    .font(.subheadline).bold()
+                    .font(.subheadline)
+                    .bold()
                     .frame(width: 120, height: 32)
                     .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 0.75))
                     .foregroundColor(Color.black)
@@ -70,5 +76,64 @@ extension ProfileView{
             
         }
         .padding(.trailing)
+    }
+    
+    var UserInfoDetail: some View {
+        VStack(alignment: .leading, spacing: 4){
+            
+            //MARK: Name, Username and Bio
+            HStack{
+                Text("Sam Grover")
+                    .font(.title2)
+                    .bold()
+                Image(systemName: "checkmark.seal.fill")
+                    .foregroundColor(Color.blue)
+            }
+            Text("@Ironman")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            Text("You Know who I am")
+                .font(.subheadline)
+                .padding(.vertical)
+                .multilineTextAlignment(.leading)
+            
+            //MARK: Location and link
+            HStack(spacing: 24){
+                HStack{
+                    Image(systemName: "mappin.and.ellipse")
+                    Text("Hisar, Haryana")
+                }
+                
+                HStack{
+                    Image(systemName: "link")
+                    Text("https://github.com/SamGrover-here")
+                }
+            }
+            .foregroundColor(.gray)
+            .font(.caption)
+            
+            //MARK: Follower and Following
+            HStack(spacing: 24){
+                
+                HStack(spacing: 4){
+                    Text("10")
+                        .bold()
+                        .font(.subheadline)
+                    Text("Following")
+                        .foregroundColor(.gray)
+                        .font(.caption)
+                }
+                HStack(spacing: 4){
+                    Text("20k")
+                        .bold()
+                        .font(.subheadline)
+                    Text("Followers")
+                        .foregroundColor(.gray)
+                        .font(.caption)
+                }
+            }.padding(.vertical)
+            
+        }.padding(.horizontal, 20)
     }
 }
